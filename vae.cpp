607ccc7407a70e2c5630e787e298c664333dbae1
divya28jain <jain28divya@gmail.com>
2023-09-26 18:10:04 +0530

    firstcommit

---
+++ vae.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,sum,b;
+
+cout<<"this is my first practice with var.\tvalue of a is";
+cout<<"\nblossom is blooming";
+cin>>a;
+cin>>b;
+cout<<"sum equals\n"<<a+b;
+return 0;
+}
